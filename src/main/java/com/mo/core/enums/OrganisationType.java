a98c87b69fed7dfc7e3a86cf0b574731f1b83001
mosanbusiness1-ship-it <mosanbusiness1@gmail.com>
2026-08-26 00:20:53 +0200

    enregistrement de OrganisationType

--- src/main/java/com/mo/core/enums/OrganisationType.java
+++ src/main/java/com/mo/core/enums/OrganisationType.java
@@ -1,12 +1,6 @@
 package com.mo.core.enums;
 
 public enum OrganisationType {
-    SALES_COMMUNITY,
-    SALES_GROUP,
-    MARKETING_TEAM,
-    DISTRIBUTION_NETWORK,
-    ROOT,
-    REALSTATE_GROUP
-    
-    // Ajoutez d'autres types selon vos besoins
+    COMMUNITY,
+    GROUP
 }
